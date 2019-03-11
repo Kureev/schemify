28ac3174eb5dc6a4f1c55a326303128bc4635900
Alexey Kureev <kureev@alexeys-mbp.mynet>
2019-03-11 21:56:15 +0000

    Remove .js files from git

--- visitors/Identifier.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function IdentifierVisitor(node) {
-    return 'identifier with a name ' + node.escapedText;
-}
-exports["default"] = IdentifierVisitor;
--- visitors/SourceFile.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function IdentifierVisitor(node) {
-    return 'file with a name ' + node.fileName;
-}
-exports["default"] = IdentifierVisitor;
