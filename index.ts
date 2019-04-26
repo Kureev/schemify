d078aaa8e3ab9f14513691f9141e4b99709ff9f4
Alexey Kureev <a.g.kureev@gmail.com>
2019-04-26 17:50:07 +0100

    Write result schema to a file

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import Transpiler from './src';
+import * as fs from 'fs';
 
-const filenames: Array<string> = process.argv.slice(2, process.argv.length);
+const [outfile, ...filenames]: Array<string> = process.argv.slice(2);
 const schema = new Transpiler(filenames).getSchema();
 
-console.log(JSON.stringify(schema));
+const formattedSchema = JSON.stringify(schema, null, 2);
+if (outfile == null) {
+  throw Error(
+    'Schemify requires two parameters: <outfile> <array of filenames>'
+  );
+}
+fs.writeFileSync(outfile, formattedSchema);
